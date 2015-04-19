0c7ff5879be7770449d6fa6f7ac7ef06004108b6
Ramakrishna Mandadapu <ramakrishnamandadapu@users.noreply.github.com>
2015-04-18 21:50:40 -0700

    Update ShoppingController.java

--- src/main/java/com/cmpe273/controller/ShoppingController.java
+++ src/main/java/com/cmpe273/controller/ShoppingController.java
@@ -1,5 +1,8 @@
 package com.cmpe273.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,47 +15,42 @@ public class ShoppingController {
 	
 	@RequestMapping(value="getoffers/beaconid/{beaconId}/rss/{rss}",method = RequestMethod.GET)
 	public Document getOffers(@PathVariable int beaconId,@PathVariable int rss){
+		
 		Document offers=new Document();
+		offers.append("beacon_id", beaconId);
+		offers.append("rss", rss);
+		List<String> offersList=new ArrayList<String>();
 		if(rss>0&&rss<=40)
 		{
-			
-			offers.append("OffersSet", "1");
-			offers.append("1", "40% on Boost");
-			offers.append("2", "20% on Horlicks");
-			offers.append("3", "10% on Bournvita");
+			offersList.add("40% on Boost");
+			offersList.add("20% on Horlicks");
+			offersList.add("10% on Bournvita");
 		}
 		if(rss>40&&rss<=50)
 		{
-			
-			offers.append("OffersSet", "1");
-			offers.append("1", "2$ on Tomatoes");
-			offers.append("2", "1$ on Brinjal");
-			offers.append("3", "3$ on Potatoes");
+			offersList.add("2$ on Tomatoes");
+			offersList.add("1$ on Brinjal");
+			offersList.add("3$ on Potatoes");
 		}
 		if(rss>50&&rss<=60)
 		{
-			
-			offers.append("OffersSet", "1");
-			offers.append("1", "40% on Reebok");
-			offers.append("2", "20% on Nike");
-			offers.append("3", "10% on Puma");
+			offersList.add("40% on Reebok");
+			offersList.add("20% on Nike");
+			offersList.add("10% on Puma");
 		}
 		if(rss>60&&rss<=80)
 		{
-			
-			offers.append("OffersSet", "1");
-			offers.append("1", "30$ on AT&T");
-			offers.append("2", "50$ on TMobile");
-			offers.append("3", "45$ on Verizon");
+			offersList.add("30$ on AT&T");
+			offersList.add("50$ on TMobile");
+			offersList.add("45$ on Verizon");
 		}
 		if(rss>80)
 		{
-			
-			offers.append("OffersSet", "1");
-			offers.append("1", "20% on HP");
-			offers.append("2", "10% on DELL");
-			offers.append("3", "70% on Acer");
+			offersList.add("20% on HP");
+			offersList.add("10% on DELL");
+			offersList.add("70% on Acer");	
 		}
+		offers.append("offer", offersList);
 		return offers;
 	}
 
