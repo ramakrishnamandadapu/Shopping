76cb8a782fa1e9fcdac7f2176ae9686d09a09064
ramakrishnamandadapu <ramakrishna.mandadapu@sjsu.edu>
2015-04-30 18:43:45 -0700

    added offer post api

--- src/main/java/com/cmpe273/controller/ShoppingController.java
+++ src/main/java/com/cmpe273/controller/ShoppingController.java
@@ -1,35 +1,45 @@
 package com.cmpe273.controller;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.cmpe273.database.DatabaseConnection;
-import com.mongodb.client.MongoCollection;
 import org.bson.Document;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cmpe273.dao.OffersDAO;
+import com.cmpe273.database.DatabaseConnection;
+import com.cmpe273.model.Offer;
+import com.mongodb.client.MongoCollection;
+
 @RestController
-@RequestMapping("/theshop/api/v1/")
+@RequestMapping("/theshop/api/v1")
 public class ShoppingController {
     private static DatabaseConnection dbConnection = new DatabaseConnection();
-    public MongoCollection<Document> collection;
+    public MongoCollection<Document> offersCollection;
+    public MongoCollection<Document> usersCollection;
 
+    @Autowired
+    public OffersDAO offersDAO;
     static{
         dbConnection.setDbConnection();
         // new ScheduledTasks();
     }
-	@RequestMapping(value="getoffers/beaconid/{beaconId}/rss/{rss}",method = RequestMethod.GET)
-	public ResponseEntity<List<Document>> getOffers(@PathVariable int beaconId,@PathVariable int rss){
-
-        this.collection = dbConnection.getCollection("offers");
-        List<Document> list = (List<Document>)this.collection.find(new Document()).into(new ArrayList<Document>());
-        return new ResponseEntity<List<Document>>(list, HttpStatus.OK);
+	@RequestMapping(value="/getoffers/beaconid/{beaconId}/rss/{rss}",method = RequestMethod.GET)
+	public List<Document> getOffers(@PathVariable String beaconId,@PathVariable int rss,@RequestParam("uid") String userId){
+        return offersDAO.getOffers(beaconId,rss,userId);
+        
+	}
+	
+	@RequestMapping(value="/postoffer",method = RequestMethod.POST)
+	public void getOffers(@RequestBody Offer offer){
+         offersDAO.postOffer(offer);
+        
 	}
 
 
